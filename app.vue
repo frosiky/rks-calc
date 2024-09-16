b67c594906474b6f8a78cca7c7482467397d90fc
frosiky <hiro2929.komuro2@gmail.com>
2024-09-16 20:10:09 +0900

    初期コミット:レート計算，表のUIなど

--- app.vue
+++ app.vue
@@ -1,79 +1,146 @@
 <script setup>
 import toRate from "/functions/toRate.js";
-const charts = [
-  { name: "譜面１", rate: 15.6, score: 99.15 },
-  { name: "譜面２", rate: 15.9 },
-];
+const charts = ref([
+  { name: "譜面１", rate: 15.6, score: 98.76 },
+  { name: "譜面２", rate: 15.9, score: 99.68 },
+  { name: "譜面３", rate: 15.9, score: 99.68 },
+]);
+let name_charts = charts.value.map((e) => e.name);
 const best = useCookie("best");
-const add = ref({name: "", rate : "", score:""})
-const add_border_color = ref({name: "gray", rate: "gray"});
-const addChart = () => {
-    add_border_color.value.name =(!add.value.name) ? "red" : ""
-  if (!add.value.rate) {
-    add_border_color.value.rate = "red"
+best.value = best.value || charts.value;
+const sync = () => {
+  for (const chart of best.value) {
+    const i = name_charts.indexOf(chart.name);
+    if (i == -1) {
+      charts.value.push({
+        name: chart.name,
+        rate: chart.rate,
+        score: chart.score ?? null,
+      });
+    } else {
+      charts.value[i] = {
+        name: chart.name,
+        rate: chart.rate,
+        score: chart.score ?? null,
+      };
+    }
   }
+  name_charts = charts.value.map((e) => e.name);
 };
+sync();
+const add = ref({ name: "", rate: "", score: "" });
+const add_border_color = ref({ name: "gray", rate: "gray" });
+const addChart = () => {
+  add_border_color.value.name = !add.value.name.length ? "red" : "gray";
+  add_border_color.value.rate = !add.value.rate.length ? "red" : "gray";
+  if (!(add.value.name.length && add.value.rate.length)) return;
+  let suffix = "";
+  if (name_charts.includes(add.value.name)) {
+    for (let i = 2; true; i++) {
+      if (!name_charts.includes(add.value.name + ` (${i})`)) {
+        suffix = ` (${i})`;
+        break;
+      }
+    }
+  }
+  best.value.push({
+    name: add.value.name + suffix,
+    rate: add.value.rate,
+    score: add.value.score.length ? add.value.score : null,
+  });
+  sync();
+};
+const deleteChart = (index) => {
+  charts.splice(index, 1)
+}
 </script>
 
 <template>
   <h1 style="font-size: 2.5rem">Phigros レート計算用</h1>
   <div id="charts">
     <table id="edit">
-      <tr>
-        <th>譜面名</th>
-        <th>定数</th>
-        <th>自己ベスト (%)</th>
-        <th>単曲rks</th>
-        <th>順位</th>
-      </tr>
-      <tr class="chart" v-for="chart in charts">
-        <td class="data">
-          {{ chart.name }}
-        </td>
-        <td class="data">
-          {{ chart.rate }}
-        </td>
-        <td class="data">
-          <input
-            class="input"
-            type="text"
-            placeholder="未入力"
-            :value="chart.score"
-          />
-        </td>
-        <td class="data">
-          {{
-            chart.score
-              ? Math.floor(toRate(chart.score / 100) * chart.rate * 100) / 100
-              : "―"
-          }}
-        </td>
-        <td class="data">{{ chart.rank ?? "―" }}</td>
-      </tr>
-      <tr style="height: 10px"></tr>
-      <tr id="add">
-        <td class="data" :style="`border: solid 2px ${add_border_color.name}`">
-          <input
-            id="add_name"
-            class="input"
-            type="text"
-            :placeholder="`未入力`"
-          />
-        </td>
-        <td class="data" :style="`border: solid 2px ${add_border_color.rate}`">
-          <input id="add_rate" class="input" type="text" placeholder="未入力" />
-        </td>
-        <td class="data">
-          <input
-            id="add_score"
-            class="input"
-            type="text"
-            placeholder="未入力"
-          />
-        </td>
-        <td class="data">―</td>
-        <td class="data hv-gr clm">追加</td>
-      </tr>
+      <tbody>
+        <tr>
+          <th>譜面名</th>
+          <th>定数</th>
+          <th>自己ベスト (%)</th>
+          <th>単曲rks</th>
+        </tr>
+        <tr class="chart" v-for="(chart, i) in charts">
+          <td class="data">
+            <input
+              class="input"
+              type="text"
+              :value="chart.name"
+              @input="(event) => (best[i].name = event.target.value.length ? event.target.value : charts[i].name)"
+            />
+          </td>
+          <td class="data">
+            <input class="input" type="number" :value="chart.rate" 
+             />
+          </td>
+          <td class="data">
+            <input
+              class="input"
+              type="number"
+              placeholder="未入力"
+              :value="chart.score"
+            />
+          </td>
+          <td class="data">
+            {{
+              chart.score
+                ? Math.round(toRate(chart.score / 100) * chart.rate * 100) / 100
+                : "―"
+            }}
+          </td>
+          <td class="data hv-re clm" @click="deleteChart(i)">消去</td>
+        </tr>
+        <tr style="height: 10px"></tr>
+        <tr id="add">
+          <td
+            class="data"
+            :style="`border: solid 2px ${add_border_color.name}`"
+          >
+            <input
+              id="add_name"
+              class="input"
+              type="text"
+              :placeholder="`未入力`"
+              @input="(event) => (add.name = event.target.value)"
+            />
+          </td>
+          <td
+            class="data"
+            :style="`border: solid 2px ${add_border_color.rate}`"
+          >
+            <input
+              id="add_rate"
+              class="input"
+              type="number"
+              placeholder="未入力"
+              @input="(event) => (add.rate = event.target.value)"
+            />
+          </td>
+          <td class="data">
+            <input
+              id="add_score"
+              class="input"
+              type="number"
+              placeholder="未入力"
+              @input="(event) => (add.score = event.target.value)"
+            />
+          </td>
+          <td class="data">
+            {{
+              add.score
+                ? Math.round(toRate(add.score / 100) * add.rate * 100) / 100
+                : "―"
+            }}
+          </td>
+          <td class="data hv-gr clm" @click="addChart()">追加</td>
+        </tr>
+      </tbody>
     </table>
   </div>
 </template>
@@ -107,6 +174,11 @@ const addChart = () => {
 
 .hv-gr:hover {
   background-color: #28952478;
+  transition: background-color 0.5s;
+}
+
+.hv-re:hover {
+  background-color: #95242478;
   transition: background-color 0.5s;
 }
 </style>
--- functions/toRate.js
+++ functions/toRate.js
@@ -1,1 +1,1 @@
-export default acc => (acc - 0.55) * (acc - 0.55) / 0.2025
+export default acc => acc >= 0.7 ? (acc - 0.55) * (acc - 0.55) / 0.2025 : 0
